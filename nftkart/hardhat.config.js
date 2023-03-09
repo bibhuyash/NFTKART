d1469bb68e315c1a0aa7f76f87453eaefdef6b87
bibhuyash <vibhakaryashaswi@gmail.com>
2023-03-09 19:44:49 +0530

    updated  hardhat network config

--- nftkart/hardhat.config.js
+++ nftkart/hardhat.config.js
@@ -3,14 +3,18 @@ require('dotenv').config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  networks:{
-    hardhat:{
-      chainId:1337
+  networks: {
+    hardhat: {
+      chainId: 1337
     },
     mainnet: {
-      url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`, // or any other JSON-RPC provider
-      accounts: process.env.SECRET_KEY
-  }
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.SECRET]
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.SECRET]
+    }
   },
   solidity: "0.8.18",
 };
