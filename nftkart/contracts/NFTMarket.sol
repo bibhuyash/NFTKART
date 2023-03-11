a4345e38168fbca13e0ea6d7efa62aade85ad7f0
bibhuyash <vibhakaryashaswi@gmail.com>
2023-03-11 17:29:25 +0530

    Created struct for Market Item in NFTMarket Contract

--- nftkart/contracts/NFTMarket.sol
+++ nftkart/contracts/NFTMarket.sol
@@ -1,6 +1,30 @@
 //SPDX-Licence-Identifier : MIT
- pragma solidity ^0.8.18;
+ pragma solidity ^0.8.14;
 
-contract NFTMarket {
-    
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract NFTMarket is ReentrancyGuard {
+    using Counters for Counters.Counter;
+    Counters.Counter private _itemId;
+    Counters.Counter private _itemSold;
+
+    address payable owner;
+    uint256 listingPrice = 0.025 ether;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    struct MarketItem {
+        uint256 itemId;
+        address nftContract;
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+    }
+
 }
