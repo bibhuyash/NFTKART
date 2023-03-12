3cf6971691b89d60553e989ebfd9205ebc54670c
bibhuyash <vibhakaryashaswi@gmail.com>
2023-03-12 12:10:03 +0530

    created home page

--- nftkart/pages/index.js
+++ nftkart/pages/index.js
@@ -57,9 +57,28 @@ export default function Home() {
   );
 
   return (
-    <div>
-      <h1></h1>
-
+    <div className="flex justify-center">
+      <div className="px-4" style={{ maxWidth: '1600px' }}>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+          {
+            nfts.map((nft, i) => (
+              <div key={i} className="border shadow rounded-xl overflow-hidden">
+                <img src={nft.image} />
+                <div className="p-4">
+                  <p style={{ height: '64px' }} className="text-2xl font-semibold">{nft.name}</p>
+                  <div style={{ height: '70px', overflow: 'hidden' }}>
+                    <p className="text-gray-400">{nft.description}</p>
+                  </div>
+                </div>
+                <div className="p-4 bg-black">
+                  <p className="text-2xl font-bold text-white">{nft.price} ETH</p>
+                  <button className="mt-4 w-full bg-pink-500 text-white font-bold py-2 px-12 rounded" onClick={() => buyNft(nft)}>Buy</button>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
     </div>
   )
 }
