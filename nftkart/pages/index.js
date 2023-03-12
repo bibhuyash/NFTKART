688dabc054536ec9199018e0cbac16b78b9daf98
bibhuyash <vibhakaryashaswi@gmail.com>
2023-03-12 12:26:49 +0530

    updated index.js and deploy.js file

--- nftkart/pages/index.js
+++ nftkart/pages/index.js
@@ -2,29 +2,33 @@ import { ethers } from 'ethers'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import Web3Modal from 'web3modal'
-import { nftaddress, nftmarketaddress } from '../config'
 
-import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
-import Market from '../artifacts/contracts/NFTMarket.sol/NFTMarketplace.json'
+import {
+  marketplaceAddress
+} from '../config'
+
+import NFTMarketplace from '../artifacts/contracts/NFTMarket.sol/NFTMarketplace.json'
 
 export default function Home() {
-  const [nfts, setNfts] = useState([]);
+  const [nfts, setNfts] = useState([])
   const [loadingState, setLoadingState] = useState('not-loaded')
-
   useEffect(() => {
-    loadNFTs();
-  }, []);
-
+    loadNFTs()
+  }, [])
   async function loadNFTs() {
-    const provider = new ethers.providers.JsonRpcProvider();
-    const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider);
-    const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, provider)
-    const data = await marketContract.fetchMarketItems();
-    console.log(data);
+    /* create a generic provider and query for unsold market items */
+    const provider = new ethers.providers.JsonRpcProvider()
+    const contract = new ethers.Contract(marketplaceAddress, NFTMarketplace.abi, provider)
+    const data = await contract.fetchMarketItems()
+
+    /*
+    *  map over items returned from smart contract and format 
+    *  them as well as fetch their token metadata
+    */
     const items = await Promise.all(data.map(async i => {
-      const tokenUri = await tokenContract.tokenURI(i.tokenId);
-      const meta = await axios.get(tokenUri);
-      const price = ethers.utils.formatUnits(i.price.toString(), 'ether');
+      const tokenUri = await contract.tokenURI(i.tokenId)
+      const meta = await axios.get(tokenUri)
+      let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
       let item = {
         price,
         tokenId: i.tokenId.toNumber(),
@@ -34,28 +38,28 @@ export default function Home() {
         name: meta.data.name,
         description: meta.data.description,
       }
-      return item;
+      return item
     }))
-    setNfts(items);
-    setLoadingState('loaded')
+    setNfts(items)
+    setLoadingState('loaded') 
   }
-
   async function buyNft(nft) {
-    const web3modal = new Web3Modal();
-    const connection = await web3modal.connect()
-    const provider = new ethers.providers.Web3Provider(connection);
-    const signer = provider.getSigner();
-    const contract = new ethers.Contract(nftmarketaddress, Market.abi, signer)
-    const price = ethers.utils.parseUnits(nft.price.toString(), 'ether')
-    const transaction = await contract.createMarketSale(nftaddress, nft.tokenId, { value: price })
-    await transaction.wait();
-    loadNFTs();
+    /* needs the user to sign the transaction, so will use Web3Provider and sign it */
+    const web3Modal = new Web3Modal()
+    const connection = await web3Modal.connect()
+    const provider = new ethers.providers.Web3Provider(connection)
+    const signer = provider.getSigner()
+    const contract = new ethers.Contract(marketplaceAddress, NFTMarketplace.abi, signer)
+
+    /* user will be prompted to pay the asking proces to complete the transaction */
+    const price = ethers.utils.parseUnits(nft.price.toString(), 'ether')   
+    const transaction = await contract.createMarketSale(nft.tokenId, {
+      value: price
+    })
+    await transaction.wait()
+    loadNFTs()
   }
-
-  if (loadingState === 'loaded' && !nfts.length) return (
-    <h1 className='px-20 py-10 text-3xl'>No items in NFTKART</h1>
-  );
-
+  if (loadingState === 'loaded' && !nfts.length) return (<h1 className="px-20 py-10 text-3xl">No items in marketplace</h1>)
   return (
     <div className="flex justify-center">
       <div className="px-4" style={{ maxWidth: '1600px' }}>
