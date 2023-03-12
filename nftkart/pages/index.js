f2f469b562de07d72edcf6a642228a2dcfdbe1ab
bibhuyash <vibhakaryashaswi@gmail.com>
2023-03-12 11:22:42 +0530

    created loadNFts method in index.js file

--- nftkart/pages/index.js
+++ nftkart/pages/index.js
@@ -1,10 +1,51 @@
-import styles from '../styles/Home.module.css'
+import { ethers } from 'ethers'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import Web3Modal from 'web3modal'
+import { nftaddress, nftmarketaddress } from '../config'
+
+import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
+import Market from '../artifacts/contracts/NFTMarket.sol/NFTMarketplace.json'
 
 export default function Home() {
+  const [nfts, setNfts] = useState([]);
+  const [loadingState, setLoadingState] = useState('not-loaded')
+
+  useEffect(() => {
+    loadNFTs();
+  }, []);
+
+  async function loadNFTs() {
+    const provider = new ethers.providers.JsonRpcProvider();
+    const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider);
+    const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, provider)
+    const data = await marketContract.fetchMarketItems();
+    console.log(data);
+    const items = await Promise.all(data.map(async i => {
+      const tokenUri = await tokenContract.tokenURI(i.tokenId);
+      const meta = await axios.get(tokenUri);
+      const price = ethers.utils.formatUnits(i.price.toSting(), 'ether');
+      let item = {
+        price,
+        tokenId: i.tokenId.toNumber(),
+        seller: i.seller,
+        owner: i.owner,
+        image: meta.data.image,
+        name: meta.data.name,
+        description: meta.data.description,
+      }
+      return item;
+    }))
+    setNfts(items);
+    setLoadingState('loaded')
+  }
+
+  if(loadingState === 'loaded' && !nfts.length) return(<h1 className='px-20 py-10 text-3xl'>No items in NFTKART</h1>);
+
   return (
-    <div className={styles.container}>
+    <div>
       <h1></h1>
-     
+
     </div>
   )
 }
